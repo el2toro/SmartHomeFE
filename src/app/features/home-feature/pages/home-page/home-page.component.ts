61509563af39b3026c7e6b0196d7432d92aefd4e
el2toro <vasea.badragan@gmail.com>
2025-03-05 20:20:45 +0100

    use components in home page

--- src/app/features/home-feature/pages/home-page/home-page.component.ts
+++ src/app/features/home-feature/pages/home-page/home-page.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { LeftSideNavbarComponent } from "../../components/left-side-navbar/left-side-navbar.component";
+import { MainDashboardComponent } from "../../components/main-dashboard/main-dashboard.component";
+import { BottomNavbarComponent } from "../../components/bottom-navbar/bottom-navbar.component";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  imports: [MatIconModule, LeftSideNavbarComponent, MainDashboardComponent, BottomNavbarComponent]
 })
 export class HomePageComponent implements OnInit {
 
