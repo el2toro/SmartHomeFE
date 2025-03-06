f10c55c3e3b4bd1e853109d687775f8ae7d23688
el2toro <vasea.badragan@gmail.com>
2025-03-06 14:35:42 +0100

    use time section component

--- src/app/features/home-feature/components/main-dashboard/main-dashboard.component.ts
+++ src/app/features/home-feature/components/main-dashboard/main-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MainDashboardTimeSectionComponent } from "../main-dashboard-time-section/main-dashboard-time-section.component";
 
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
-  styleUrls: ['./main-dashboard.component.scss']
+  styleUrls: ['./main-dashboard.component.scss'],
+  imports: [MainDashboardTimeSectionComponent]
 })
 export class MainDashboardComponent implements OnInit {
 
